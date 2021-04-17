f2cfafaaf88d9d5ad11a918d4b80478dc0129e5e
DenzelWilliams <schdwilliams@gmail.com>
2021-04-16 20:21:30 -0400

    Same as Previous

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,4 @@
 --The Query to Load the Stations in the Graph Database
 
 LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Williamdst/Capstone-2/main/Data/Stations-Subset.csv' AS row fieldterminator ','
-MERGE (s:Stations {name:row.StopName, ID:row.StationID, borough:row.Borough, routes: split(row.Routes, ':'), nodes: split(row.Nodes, ':')})
+MERGE (s:Stations {name:row.StopName, id:row.StationID, borough:row.Borough, routes: split(row.Routes, ':'), nodes: split(row.Nodes, ':')})
