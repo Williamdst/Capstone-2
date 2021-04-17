7835f825e0d72ac9b0332e780f3025310182035f
DenzelWilliams <schdwilliams@gmail.com>
2021-04-16 21:13:46 -0400

    Updated Paths

--- Queries.sql
+++ Queries.sql
@@ -2,3 +2,11 @@
 
 LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Williamdst/Capstone-2/main/Data/Stations-Subset.csv' AS row fieldterminator ','
 MERGE (s:Stations {name:row.stopName, ID:row.stationID, borough:row.borough, routes: split(row.routes, ':'), nodes: split(row.nodes, ':')})
+
+
+--The Query to Load the Relationships in the Graph Database
+
+LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/Williamdst/Capstone-2/main/Data/Paths.csv" AS row
+match (s1:Stations {ID:row.startID})
+match (s2:Stations {ID:row.stopID})
+merge (s1)-[adj:Adjacent]->(s2)
