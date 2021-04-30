11eaf18ee14d5b0142d6c39cfa1dcda39444b18d
DenzelWilliams <schdwilliams@gmail.com>
2021-04-30 12:17:31 -0400

    Temporarily Removed Distance

--- The-Subway-Challenge/Queries.sql
+++ The-Subway-Challenge/Queries.sql
@@ -1,23 +1,23 @@
 //The Query to Load the Stations in the Graph Database
 
-LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Williamdst/Capstone-2/main/The-Subway-Challenge/Stations-Decision-Points.csv' AS row
+LOAD CSV WITH HEADERS FROM 'https://raw.githubusercontent.com/Williamdst/Capstone-2/main/Data/Stations-Decision-Points.csv' AS row
 MERGE (s:Stations {name:row.stopName, ID:toInteger(row.stationID), borough:row.borough, lines: split(row.lines, ':'), nodes: split(row.nodes, ':')});
 
 
 //The Query to Load the Relationships in the Graph Database
 
-LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/Williamdst/Capstone-2/main/The-Subway-Challenge/Paths-Decision-Points.csv" AS row
+LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/Williamdst/Capstone-2/main/Data/Paths-Decision-Points.csv" AS row
 MATCH (s1:Stations {ID:toInteger(row.startID)})
 MATCH (s2:Stations {ID:toInteger(row.stopID)})
-MERGE (s1)-[rel:LINK {routes:split(row.routes, ':'), cost:toInteger(row.distance)}]->(s2);
+MERGE (s1)-[rel:LINK {routes:split(row.routes, ':')}]->(s2);
 
 //The Query is the same, but in the file the headers are switched and modifications are made
 //LOAD CSV WITH HEADERS FROM "https://raw.githubusercontent.com/Williamdst/Capstone-2/main/Data/Paths-Backward.csv" AS row
 //MATCH (s1:Stations {ID:row.startID})
 //MATCH (s2:Stations {ID:row.stopID})
-//MERGE (s1)-[rel:LINK]->(s2);
+//MERGE (s1)-[rel:LINK {routes:split(row.routes, ':'), cost:toInteger(row.distance)}]->(s2);
 
-//Add Labels to the Nodes
+Add Labels to the Nodes
 MATCH (s1:Stations)
 WHERE 'A' in s1.lines
 SET s1:A;
